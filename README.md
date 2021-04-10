5c6c149044eb21220619769fa48d4e904589f525
lxxjn0 <ljy3510@gmail.com>
2021-04-10 17:51:51 +0900

    feat(gradle): 프로젝트 초기 설정
    - JDK 11 관련 설정
    - gradle 관련 설정

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # untitled-server
+
 백엔드 개발을 진행하는 Repository
---
+++ src/main/kotlin/com/devfloor/untitled/UntitledApplication.kt
@@ -0,0 +1,11 @@
+package com.devfloor.untitled
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UntitledApplication
+
+fun main(args: Array<String>) {
+    runApplication<UntitledApplication>(*args)
+}
