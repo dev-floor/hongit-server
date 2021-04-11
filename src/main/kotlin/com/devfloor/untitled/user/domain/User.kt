0c030d347849d27a3d9465011b947f09844c9d76
Junyoung Lee <ljy3510@gmail.com>
2021-04-11 12:49:25 +0900

    feature(user): 유저 엔티티 구현 (#8)
    * feat(user): User 엔티티 구현
    
    - 재학생, 졸업생을 구분하는 UserType 구현
    
    * feat(user): UserRepository 구현
    
    * refactor(user): User 프로퍼티 수정
    
    - val에서 var로 변경
    - 몇가지 프로퍼티의 명명 변경 및 추가
    
    * docs(user): User와 관련된 클래스 주석 작성
    
    * refactor(user): classOf 컬럼에 Unique 제약조건 추가

---
+++ src/main/kotlin/com/devfloor/untitled/user/domain/User.kt
@@ -0,0 +1,67 @@
+package com.devfloor.untitled.user.domain
+
+import javax.persistence.*
+
+/**
+ * 회원 정보를 관리하는 entity
+ *
+ * @property id 아이디
+ * @property nickname 닉네임
+ * @property type 유형
+ * @property image 프로필 사진
+ * @property classOf 학번
+ * @property github 깃허브 주소
+ * @property blog 블로그 주소
+ * @property description 추가 설명
+ */
+@Entity
+@Table(
+    name = "user",
+    uniqueConstraints = [
+        UniqueConstraint(name = "uk_user_1", columnNames = ["classOf"]),
+    ],
+)
+class User(
+    nickname: String,
+    type: UserType,
+    image: String,
+    classOf: String?,
+    github: String?,
+    blog: String?,
+    description: String?,
+) {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    var id: Long? = null
+        private set
+
+    @Column(name = "name")
+    var nickname: String = nickname
+        private set
+
+    @Enumerated(value = EnumType.STRING)
+    @Column(name = "type")
+    var type: UserType = type
+        private set
+
+    @Column(name = "image")
+    var image: String = image
+        private set
+
+    @Column(name = "class_of")
+    var classOf: String? = classOf
+        private set
+
+    @Column(name = "github")
+    var github: String? = github
+        private set
+
+    @Column(name = "blog")
+    var blog: String? = blog
+        private set
+
+    @Column(name = "description")
+    var description: String? = description
+        private set
+}
