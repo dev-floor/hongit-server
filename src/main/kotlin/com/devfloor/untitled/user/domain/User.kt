b6ee16a713866b1be1b777adc28fd4649b30e077
lxxjn0 <ljy3510@gmail.com>
2021-04-11 12:10:57 +0900

    refactor(user): User 프로퍼티 수정
    - val에서 var로 변경
    - 몇가지 프로퍼티의 명명 변경 및 추가

--- src/main/kotlin/com/devfloor/untitled/user/domain/User.kt
+++ src/main/kotlin/com/devfloor/untitled/user/domain/User.kt
@@ -5,40 +5,47 @@ import javax.persistence.*
 @Entity
 @Table(name = "user")
 class User(
-    id: Long = 0,
-    name: String,
+    nickname: String,
     type: UserType,
-    image: String = "",
-    classOf: String? = null,
-    github: String? = null,
-    blog: String? = null,
-    description: String? = null,
+    image: String,
+    classOf: String?,
+    github: String?,
+    blog: String?,
+    description: String?,
 ) {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
-    val id: Long = id
+    var id: Long? = null
+        private set
 
     @Column(name = "name")
-    val name: String = name
+    var nickname: String = nickname
+        private set
 
     @Enumerated(value = EnumType.STRING)
     @Column(name = "type")
-    val type: UserType = type
+    var type: UserType = type
+        private set
 
     @Column(name = "image")
-    val image: String = image
+    var image: String = image
+        private set
 
     @Column(name = "class_of")
-    val classOf: String? = classOf
+    var classOf: String? = classOf
+        private set
 
     @Column(name = "github")
-    val github: String? = github
+    var github: String? = github
+        private set
 
     @Column(name = "blog")
-    val blog: String? = blog
+    var blog: String? = blog
+        private set
 
     @Lob
     @Column(name = "description")
-    val description: String? = description
+    var description: String? = description
+        private set
 }
