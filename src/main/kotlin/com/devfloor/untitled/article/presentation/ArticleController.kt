e0b5a517ebfc25fac6b5d51039494ba21d4f6089
hungryjay <juwonson2068@naver.com>
2021-04-13 00:37:07 +0900

    feat(article): get /articles/{id} API 정의

--- src/main/kotlin/com/devfloor/untitled/article/presentation/ArticleController.kt
+++ src/main/kotlin/com/devfloor/untitled/article/presentation/ArticleController.kt
@@ -1,4 +1,14 @@
 package com.devfloor.untitled.article.presentation
 
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
 class ArticleController {
+
+    @GetMapping("/articles/{id}")
+    fun article(@PathVariable id: String) {
+        TODO()
+    }
 }
