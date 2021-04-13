a63e0fd09b63013fd2ec889e620a8b95df926eec
joowon_son <joowon_son@tmax.co.kr>
2021-04-13 18:09:31 +0900

    refactor(article): 빌트인 Extension 이용해 Optional<T> 제거

--- src/main/kotlin/com/devfloor/untitled/article/application/ArticleService.kt
+++ src/main/kotlin/com/devfloor/untitled/article/application/ArticleService.kt
@@ -1,10 +1,12 @@
 package com.devfloor.untitled.article.application
 
+import com.devfloor.untitled.article.domain.Article
 import com.devfloor.untitled.article.domain.ArticleRepository
+import org.springframework.data.repository.findByIdOrNull
 
 class ArticleService(
         private val repository: ArticleRepository
 ) {
 
-    fun getArticle(id: Long) = repository.findById(id)
+    fun findById(id: Long): Article? = repository.findByIdOrNull(id)
 }
