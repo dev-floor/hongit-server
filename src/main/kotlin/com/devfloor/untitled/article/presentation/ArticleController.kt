e6df2f749ce326cbb168c8e97d9fd398eea52f10
hungry-jay <juwonson2068@naver.com>
2021-04-14 18:09:03 +0900

    feat(article): controller 로직 구현

--- src/main/kotlin/com/devfloor/untitled/article/presentation/ArticleController.kt
+++ src/main/kotlin/com/devfloor/untitled/article/presentation/ArticleController.kt
@@ -1,14 +1,24 @@
 package com.devfloor.untitled.article.presentation
 
+import com.devfloor.untitled.article.application.ArticleFacade
+import com.devfloor.untitled.article.application.GetArticleResponse
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class ArticleController {
+class ArticleController(
+        private val articleFacade: ArticleFacade
+) {
 
     @GetMapping("/articles/{article_id}")
-    fun getArticle(@PathVariable id: String) {
-        TODO()
+    fun getArticle(@PathVariable id: Long): ResponseEntity<GetArticleResponse> {
+        return try {
+            ResponseEntity(articleFacade.getArticle(id), HttpStatus.OK)
+        } catch (e: IllegalArgumentException) {
+            ResponseEntity(HttpStatus.NOT_FOUND)
+        }
     }
 }
