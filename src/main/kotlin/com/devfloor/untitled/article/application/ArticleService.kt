96f8e067edc69b2899af59c7c3c3f0aec416ef21
hungry-jay <juwonson2068@naver.com>
2021-04-14 17:26:12 +0900

    feat(annotation): service, facade 어노테이션 추가

--- src/main/kotlin/com/devfloor/untitled/article/application/ArticleService.kt
+++ src/main/kotlin/com/devfloor/untitled/article/application/ArticleService.kt
@@ -3,8 +3,10 @@ package com.devfloor.untitled.article.application
 import com.devfloor.untitled.article.domain.Article
 import com.devfloor.untitled.article.domain.ArticleRepository
 import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Service
 import java.lang.IllegalArgumentException
 
+@Service
 class ArticleService(
         private val repository: ArticleRepository
 ) {
