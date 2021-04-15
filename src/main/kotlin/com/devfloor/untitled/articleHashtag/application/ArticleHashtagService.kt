1d15ba971f3f12b9a453dbf94a388e91b3e1b834
hungryjay <juwonson2068@naver.com>
2021-04-15 16:00:33 +0900

    refactor: @Transactional 어노테이션 추가

--- src/main/kotlin/com/devfloor/untitled/articleHashtag/application/ArticleHashtagService.kt
+++ src/main/kotlin/com/devfloor/untitled/articleHashtag/application/ArticleHashtagService.kt
@@ -4,11 +4,13 @@ import com.devfloor.untitled.article.domain.Article
 import com.devfloor.untitled.articleHashtag.domain.ArticleHashtag
 import com.devfloor.untitled.articleHashtag.domain.ArticleHashtagRepository
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class ArticleHashtagService(
     private val repository: ArticleHashtagRepository
 ) {
+    @Transactional(readOnly = true)
     fun findByArticle(article: Article): List<ArticleHashtag> =
         repository.findAllByArticle(article) ?: emptyList()
 }
