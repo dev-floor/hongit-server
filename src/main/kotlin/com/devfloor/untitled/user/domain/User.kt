59abd313a3ae17927e658ac14f4c7d15db7885ef
hungryjay <juwonson2068@naver.com>
2021-04-16 15:06:57 +0900

    fix(user): unique 컬럼명 수정

--- src/main/kotlin/com/devfloor/untitled/user/domain/User.kt
+++ src/main/kotlin/com/devfloor/untitled/user/domain/User.kt
@@ -19,7 +19,7 @@ import javax.persistence.*
 @Table(
     name = "user",
     uniqueConstraints = [
-        UniqueConstraint(name = "uk_user_class_of", columnNames = ["classOf"]),
+        UniqueConstraint(name = "uk_user_class_of", columnNames = ["class_of"]),
     ],
 )
 class User(
