2a2b291f5ec8bb857d39cf0ebc3c8b6b9d179c85
HungryJay <49271821+hungry-jay@users.noreply.github.com>
2021-04-21 22:43:53 +0900

    refactor: 리뷰 반영
    Co-authored-by: Junyoung Lee <ljy3510@gmail.com>

--- src/main/kotlin/com/devfloor/untitled/article/application/ArticleResponse.kt
+++ src/main/kotlin/com/devfloor/untitled/article/application/ArticleResponse.kt
@@ -5,16 +5,26 @@ import java.time.LocalDateTime
 
 data class ArticleResponse(
     val options: List<String>,
+    
     val title: String? = null,
+    
     val anonymous: Boolean,
+    
     val content: String,
+    
     val author: Any,
+    
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", shape = JsonFormat.Shape.STRING)
     val createdDate: LocalDateTime,
+    
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", shape = JsonFormat.Shape.STRING)
     val modifiedDate: LocalDateTime,
+    
     val hashtags: List<String>,
+    
     val favorites: Long,
+    
     val wonders: Long,
+    
     val clips: Long
 )
