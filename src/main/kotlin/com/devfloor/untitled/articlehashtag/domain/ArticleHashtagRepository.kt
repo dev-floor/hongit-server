0287bb068050973016833ed7886d9a175d565863
hungryjay <juwonson2068@naver.com>
2021-05-16 23:36:29 +0900

    feat: @Query + JPQL 적용

--- src/main/kotlin/com/devfloor/untitled/articlehashtag/domain/ArticleHashtagRepository.kt
+++ src/main/kotlin/com/devfloor/untitled/articlehashtag/domain/ArticleHashtagRepository.kt
@@ -4,12 +4,14 @@ import com.devfloor.untitled.article.domain.Article
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.transaction.annotation.Transactional
 import org.springframework.data.jpa.repository.Modifying
+import org.springframework.data.jpa.repository.Query
 
 @Transactional(readOnly = true)
 interface ArticleHashtagRepository : JpaRepository<ArticleHashtag, Long> {
     fun findAllByArticle(article: Article): List<ArticleHashtag>
 
     @Transactional
-    @Modifying(clearAutomatically = true)
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Query("DELETE FROM ArticleHashtag WHERE article = :article") // TODO : queryDSL 로?
     fun deleteAllByArticle(article: Article)
 }
--- src/main/kotlin/com/devfloor/untitled/articleoption/domain/ArticleOptionRepository.kt
+++ src/main/kotlin/com/devfloor/untitled/articleoption/domain/ArticleOptionRepository.kt
@@ -4,12 +4,14 @@ import com.devfloor.untitled.article.domain.Article
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.transaction.annotation.Transactional
 import org.springframework.data.jpa.repository.Modifying
+import org.springframework.data.jpa.repository.Query
 
 @Transactional(readOnly = true)
 interface ArticleOptionRepository : JpaRepository<ArticleOption, Long> {
     fun findAllByArticle(article: Article): List<ArticleOption>
 
     @Transactional
-    @Modifying(clearAutomatically = true)
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Query("DELETE FROM ArticleOption WHERE article = :article") // TODO : queryDSL 로?
     fun deleteAllByArticle(article: Article)
 }
