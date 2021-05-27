dc4249787878cd595d0a514f8b4ddfd0946dd461
lxxjn0 <ljy3510@gmail.com>
2021-05-27 23:02:13 +0900

    feat(professor): Professor entity 구현

---
+++ src/main/kotlin/com/devfloor/untitled/professor/domain/Professor.kt
@@ -0,0 +1,33 @@
+package com.devfloor.untitled.professor.domain
+
+import com.devfloor.untitled.common.domain.BaseEntity
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+
+/**
+ * 강의를 진행하는 교수의 정보를 관리하는 entity
+ *
+ * @property id 아이디
+ * @property name 이름
+ * @property email 이메일
+ */
+@Entity
+class Professor(
+    name: String,
+    email: String?,
+) : BaseEntity() {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "professor_id")
+    val id: Long = 0
+
+    @Column(name = "name")
+    val name: String = name
+
+    @Column(name = "email")
+    var email: String? = email
+        protected set
+}
