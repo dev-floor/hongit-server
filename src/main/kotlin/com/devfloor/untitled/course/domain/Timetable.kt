d6739238b6a6470e57c0544d8592394df7e0d482
lxxjn0 <ljy3510@gmail.com>
2021-06-01 00:09:44 +0900

    refactor(timetable): timetable 객체 수정
    - Timetable 객체가 schedule 리스트를 가지는 일급 컬렉션으로 수정
    - 시간표 단건을 관리하는 schedule 객체 구현

--- src/main/kotlin/com/devfloor/untitled/course/domain/Timetable.kt
+++ src/main/kotlin/com/devfloor/untitled/course/domain/Timetable.kt
@@ -1,38 +1,25 @@
 package com.devfloor.untitled.course.domain
 
+import javax.persistence.CollectionTable
 import javax.persistence.Column
+import javax.persistence.ElementCollection
 import javax.persistence.Embeddable
+import javax.persistence.JoinColumn
 
 /**
  * 수업 시간표를 관리하는 embedded 객체
  *
- * @property courseDay 수업 요일
- * @property courseTime 수업 시간
- * @property text 수업 시간표 문자열
+ * @property timetable 수업 시간표 목록
  */
 @Embeddable
 class Timetable(
-    courseDay: CourseDay,
-    courseTime: CourseTime,
+    timetable: List<Schedule>,
 ) {
-    @Column(name = "course_day")
-    val courseDay: CourseDay = courseDay
-
-    @Column(name = "course_time")
-    val courseTime: CourseTime = courseTime
-
-    val text: String
-        get() = "${courseDay.text}${courseTime.number}"
-
-    private fun verifyText(text: String): Boolean =
-        courseDay.text == text.substring(0, 1) && courseTime.number == text.substring(1).toLong()
-
-    companion object {
-        private val CACHE: List<Timetable> = enumValues<CourseDay>()
-            .flatMap { courseDay ->
-                enumValues<CourseTime>().map { Timetable(courseDay, it) }
-            }
-
-        fun from(text: String) = CACHE.first { it.verifyText(text) }
-    }
+    @ElementCollection
+    @CollectionTable(
+        name = "schedules",
+        joinColumns = [JoinColumn(name = "course_id")]
+    )
+    @Column(name = "timetable")
+    val timetable: List<Schedule> = timetable
 }
