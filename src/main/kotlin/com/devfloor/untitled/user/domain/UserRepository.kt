7a69fe2bb1fa2c5489831841d18e51b89791b904
lxxjn0 <ljy3510@gmail.com>
2021-06-03 00:32:05 +0900

    refactor(docs): UserRepository 문서화 주석 수정

--- src/main/kotlin/com/devfloor/untitled/user/domain/UserRepository.kt
+++ src/main/kotlin/com/devfloor/untitled/user/domain/UserRepository.kt
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.transaction.annotation.Transactional
 
 /**
- * User를 관리하는 Repository
+ * 회원을 관리하는 Repository
  */
 @Transactional(readOnly = true)
 interface UserRepository : JpaRepository<User, Long>
