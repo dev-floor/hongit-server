fa3e70bc6ab42c73afebdea6a8aae1f0237a86d3
lxxjn0 <ljy3510@gmail.com>
2021-06-03 18:17:26 +0900

    refactor(commentFavorite): CommentFavorite entity에 Index 설정

--- src/main/kotlin/com/devfloor/untitled/commentfavorite/domain/CommentFavorite.kt
+++ src/main/kotlin/com/devfloor/untitled/commentfavorite/domain/CommentFavorite.kt
@@ -8,6 +8,7 @@ import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
 import javax.persistence.Id
+import javax.persistence.Index
 import javax.persistence.JoinColumn
 import javax.persistence.ManyToOne
 import javax.persistence.Table
@@ -20,7 +21,12 @@ import javax.persistence.Table
  * @property user 좋아요를 누른 사람
  */
 @Entity
-@Table(name = "comment_favorites")
+@Table(
+    name = "comment_favorites",
+    indexes = [
+        Index(name = "idx_comment_id", columnList = "comment_id")
+    ]
+)
 class CommentFavorite(
     comment: Comment,
     user: User,
