6959519cd930c3ef6db9d0e61de32aa21eda563a
HungryJay <49271821+hungry-jay@users.noreply.github.com>
2021-07-12 17:30:29 +0900

    feat(articleFavorite): 게시글 좋아요 생성, 삭제 API (#48)
    * feat(ArticleFavorite): 게시글 좋아요 생성 API
    
    * feat(ArticleFavorite): 게시글 좋아요 삭제 API
    
    * style: wildcard import 제거
    
    * refactor(articleFavorite): 리뷰 반영
    
    * refactor: 리뷰(URI case, DTO) 반영
    
    * refactor: URI 수정, Docker 설정 수정
    
    * refactor: 리뷰 반영

--- src/main/kotlin/com/devfloor/untitled/hashtag/domain/HashtagRepository.kt
+++ src/main/kotlin/com/devfloor/untitled/hashtag/domain/HashtagRepository.kt
@@ -2,7 +2,7 @@ package com.devfloor.untitled.hashtag.domain
 
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.transaction.annotation.Transactional
-import java.util.*
+import java.util.Optional
 
 /**
  * 해시태그를 관리하는 repository
