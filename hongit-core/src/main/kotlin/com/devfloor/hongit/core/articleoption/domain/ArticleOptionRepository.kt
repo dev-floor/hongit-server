aaa89c4fde7b820af33c088131373e49473ba469
lxxjn0 <ljy3510@gmail.com>
2021-07-13 22:04:04 +0900

    feat(core): articleoption 패키지 이동

---
+++ hongit-core/src/main/kotlin/com/devfloor/hongit/core/articleoption/domain/ArticleOptionRepository.kt
@@ -0,0 +1,17 @@
+package com.devfloor.hongit.core.articleoption.domain
+
+import com.devfloor.hongit.core.article.domain.Article
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Modifying
+import org.springframework.data.jpa.repository.Query
+import org.springframework.transaction.annotation.Transactional
+
+@Transactional(readOnly = true)
+interface ArticleOptionRepository : JpaRepository<ArticleOption, Long> {
+    fun findAllByArticle(article: Article): List<ArticleOption>
+
+    @Transactional
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Query(value = "DELETE FROM ArticleOption WHERE article = :article")
+    fun deleteAllByArticle(article: Article)
+}
