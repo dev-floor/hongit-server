224d304894629c0b2963540c9ede1523797823f0
lxxjn0 <ljy3510@gmail.com>
2021-07-13 21:56:31 +0900

    feat(core): board 패키지 이동

---
+++ hongit-core/src/main/kotlin/com/devfloor/hongit/core/board/domain/BoardRepository.kt
@@ -0,0 +1,9 @@
+package com.devfloor.hongit.core.board.domain
+
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.transaction.annotation.Transactional
+
+@Transactional(readOnly = true)
+interface BoardRepository : JpaRepository<Board, Long> {
+    fun findAllByType(type: BoardType): List<Board>
+}
