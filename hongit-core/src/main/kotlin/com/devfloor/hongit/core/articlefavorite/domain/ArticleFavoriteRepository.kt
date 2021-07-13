071334bf586025977297f14703702777a62dd740
lxxjn0 <ljy3510@gmail.com>
2021-07-13 22:01:48 +0900

    feat(core): articlefavorite 패키지 이동

---
+++ hongit-core/src/main/kotlin/com/devfloor/hongit/core/articlefavorite/domain/ArticleFavoriteRepository.kt
@@ -0,0 +1,21 @@
+package com.devfloor.hongit.core.articlefavorite.domain
+
+import com.devfloor.hongit.core.article.domain.Article
+import com.devfloor.hongit.core.user.domain.User
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Modifying
+import org.springframework.data.jpa.repository.Query
+import org.springframework.transaction.annotation.Transactional
+
+@Transactional(readOnly = true)
+interface ArticleFavoriteRepository : JpaRepository<ArticleFavorite, Long> {
+    fun findAllByArticle(article: Article): List<ArticleFavorite>
+
+    @Transactional
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Query(value = "DELETE FROM ArticleFavorite WHERE article = :article")
+    fun deleteAllByArticle(article: Article)
+
+    @Transactional
+    fun deleteByArticleAndUser(article: Article, user: User)
+}
---
+++ hongit-core/src/main/kotlin/com/devfloor/hongit/core/articlefavorite/domain/ArticleFavoriteType.kt
@@ -0,0 +1,20 @@
+package com.devfloor.hongit.core.articlefavorite.domain
+
+import com.devfloor.hongit.core.common.domain.BaseEnum
+
+/**
+ * 게시글에 대한 좋아요의 유형을 관리하는 enum
+ *
+ * @property text 좋아요 유형
+ */
+enum class ArticleFavoriteType(
+    override val text: String,
+) : BaseEnum {
+    FAVORITE("좋아요"),
+    WONDER("나도 궁금해요"),
+    CLIP("스크랩"),
+    ;
+
+    override val id: String
+        get() = this.name
+}
