af7ffab6564c4ca5325464164e7d7946ac56b39a
hungryjay <juwonson2068@naver.com>
2021-08-01 18:08:37 +0900

    refactor: CommentInProfile 수정

--- hongit-api/src/main/kotlin/com/devfloor/hongit/api/comment/application/response/CommentInProfileResponse.kt
+++ hongit-api/src/main/kotlin/com/devfloor/hongit/api/comment/application/response/CommentInProfileResponse.kt
@@ -1,28 +1,11 @@
 package com.devfloor.hongit.api.comment.application.response
 
-import com.devfloor.hongit.api.article.presentation.ArticleController.Companion.ARTICLE_API_URI
-import com.devfloor.hongit.api.user.application.response.ProfileResponse
 import com.devfloor.hongit.core.comment.domain.Comment
-import com.fasterxml.jackson.annotation.JsonFormat
-import java.net.URI
-import java.time.LocalDateTime
 
 data class CommentInProfileResponse(
-    val id: Long,
+    val commentResponse: CommentResponse,
 
-    val author: ProfileResponse,
-
-    val content: String,
-
-    val favoriteCount: Long,
-
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", shape = JsonFormat.Shape.STRING)
-    val createdDate: LocalDateTime,
-
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", shape = JsonFormat.Shape.STRING)
-    val modifiedDate: LocalDateTime,
-
-    val articleUri: URI,
+    val articleId: Long,
 
     val articleTitle: String?,
 ) {
@@ -30,13 +13,8 @@ data class CommentInProfileResponse(
         comment: Comment,
         favoriteCount: Long = 0,
     ) : this(
-        id = comment.id,
-        author = ProfileResponse(comment.author),
-        content = comment.content,
-        favoriteCount = favoriteCount,
-        createdDate = comment.createdAt,
-        modifiedDate = comment.modifiedAt,
-        articleUri = URI.create("$ARTICLE_API_URI/${comment.article.id}"),
-        articleTitle = comment.article.title
+        commentResponse = CommentResponse(comment, favoriteCount),
+        articleId = comment.article.id,
+        articleTitle = comment.article.title,
     )
 }
