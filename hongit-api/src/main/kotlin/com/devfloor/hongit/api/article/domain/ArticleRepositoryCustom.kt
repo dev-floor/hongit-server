b9b7ecc2b63b5e80626dfb8b021d2c17ab2e5dcf
JoonHyeong Park <wnsgud245@naver.com>
2021-08-01 17:40:55 +0900

    51 홈화면 조회 api 커스텀 구현체 분리

--- hongit-api/src/main/kotlin/com/devfloor/hongit/api/article/domain/ArticleRepositoryCustom.kt
+++ hongit-api/src/main/kotlin/com/devfloor/hongit/api/article/domain/ArticleRepositoryCustom.kt
@@ -1,36 +1,8 @@
 package com.devfloor.hongit.api.article.domain
 
 import com.devfloor.hongit.core.article.domain.Article
-import com.devfloor.hongit.core.article.domain.QArticle.article
-import com.devfloor.hongit.core.articlefavorite.domain.QArticleFavorite.articleFavorite
-import com.devfloor.hongit.core.articleviewcount.domain.QArticleViewCount.articleViewCount
-import com.querydsl.jpa.impl.JPAQueryFactory
 
-import org.springframework.stereotype.Repository
-import javax.persistence.EntityManager
-
-@Repository
-class ArticleRepositoryCustom(
-    val entityManager: EntityManager,
-    val jpaQueryFactory: JPAQueryFactory,
-) {
-
-    fun findByFavoriteTopFive(): List<Article> {
-        return jpaQueryFactory.selectFrom(article)
-            .leftJoin(articleFavorite).on(article.eq(articleFavorite.article))
-            .fetchJoin()
-            .groupBy(article)
-            .orderBy(articleFavorite.count().desc())
-            .limit(5)
-            .fetch()
-    }
-
-    fun findByViewCountTopFive(): List<Article> {
-        return jpaQueryFactory.selectFrom(article)
-            .innerJoin(articleViewCount).on(article.eq(articleViewCount.article))
-            .fetchJoin()
-            .orderBy(articleViewCount.count.desc())
-            .limit(5)
-            .fetch()
-    }
+interface ArticleRepositoryCustom {
+    fun findByFavoriteTopFive(): List<Article>
+    fun findByViewCountTopFive(): List<Article>
 }
