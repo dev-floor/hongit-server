9eaa3a5117abe113c88fb554ec87fc947f01b799
HungryJay <49271821+hungry-jay@users.noreply.github.com>
2021-08-01 18:10:53 +0900

    feat(comment): 프로필 내 댓글 목록 조회 API (#64)
    * feat: 프로필 - 댓글 목록 조회 구현
    
    * refactor: 메소드 네이밍 변경
    
    * refactor: comment DTO에 article URI, title 추가
    
    * refactor: 댓글목록(프로필) 네이밍 수정
    
    * refactor: CommentInProfile 수정

--- hongit-api/src/main/kotlin/com/devfloor/hongit/api/comment/application/CommentService.kt
+++ hongit-api/src/main/kotlin/com/devfloor/hongit/api/comment/application/CommentService.kt
@@ -3,6 +3,7 @@ package com.devfloor.hongit.api.comment.application
 import com.devfloor.hongit.api.comment.application.request.CommentCreateRequest
 import com.devfloor.hongit.api.comment.application.request.CommentModifyRequest
 import com.devfloor.hongit.api.comment.application.response.CommentResponse
+import com.devfloor.hongit.api.comment.application.response.CommentInProfileResponse
 import com.devfloor.hongit.api.common.exception.EntityNotFoundException
 import com.devfloor.hongit.core.article.domain.Article
 import com.devfloor.hongit.core.article.domain.ArticleRepository
@@ -10,6 +11,7 @@ import com.devfloor.hongit.core.comment.domain.Comment
 import com.devfloor.hongit.core.comment.domain.CommentRepository
 import com.devfloor.hongit.core.commentfavorite.domain.CommentFavoriteRepository
 import com.devfloor.hongit.core.user.domain.User
+import com.devfloor.hongit.core.user.domain.UserRepository
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -19,6 +21,7 @@ class CommentService(
     private val articleRepository: ArticleRepository,
     private val commentRepository: CommentRepository,
     private val commentFavoriteRepository: CommentFavoriteRepository,
+    private val userRepository: UserRepository,
 ) {
     @Transactional(readOnly = true)
     fun showAllByArticleId(articleId: Long): List<CommentResponse> {
@@ -28,6 +31,20 @@ class CommentService(
 
         return comments.map {
             CommentResponse(
+                comment = it,
+                favoriteCount = commentFavoriteRepository.countAllByComment(it),
+            )
+        }
+    }
+
+    @Transactional(readOnly = true)
+    fun showAllByUserId(userId: Long): List<CommentInProfileResponse> {
+        val comments = userRepository.findByIdOrNull(userId)
+            ?.let(commentRepository::findAllByAuthor)
+            ?: EntityNotFoundException.notExistsId(User::class, userId)
+
+        return comments.map {
+            CommentInProfileResponse(
                 comment = it,
                 favoriteCount = commentFavoriteRepository.countAllByComment(it),
             )
--- hongit-core/src/main/kotlin/com/devfloor/hongit/core/comment/domain/CommentRepository.kt
+++ hongit-core/src/main/kotlin/com/devfloor/hongit/core/comment/domain/CommentRepository.kt
@@ -1,10 +1,13 @@
 package com.devfloor.hongit.core.comment.domain
 
 import com.devfloor.hongit.core.article.domain.Article
+import com.devfloor.hongit.core.user.domain.User
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.transaction.annotation.Transactional
 
 @Transactional(readOnly = true)
 interface CommentRepository : JpaRepository<Comment, Long> {
     fun findAllByArticle(article: Article): List<Comment>
+
+    fun findAllByAuthor(author: User): List<Comment>
 }
