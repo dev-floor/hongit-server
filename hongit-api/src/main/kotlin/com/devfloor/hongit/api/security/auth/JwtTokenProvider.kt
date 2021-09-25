340b43ba7878870cc86fdc2b84dd1ef5c8326c2c
lxxjn0 <ljy3510@gmail.com>
2021-09-25 20:35:33 +0900

    feat(auth): JwtTokenProvider 구현

---
+++ hongit-api/src/main/kotlin/com/devfloor/hongit/api/security/auth/JwtTokenProvider.kt
@@ -0,0 +1,41 @@
+package com.devfloor.hongit.api.security.auth
+
+import io.jsonwebtoken.Claims
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.SignatureAlgorithm
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.stereotype.Component
+import java.util.Date
+
+@Component
+@EnableConfigurationProperties(value = [JwtTokenProperties::class])
+class JwtTokenProvider(
+    private val properties: JwtTokenProperties,
+) {
+    fun createToken(payload: String): String {
+        val claims = Jwts.claims().setSubject(payload)
+        val currentDate = Date()
+
+        return Jwts.builder()
+            .setClaims(claims)
+            .setIssuedAt(currentDate)
+            .setExpiration(Date(currentDate.time + properties.expireTime))
+            .signWith(SignatureAlgorithm.HS256, properties.secretKey)
+            .compact()
+    }
+
+    fun extractPayload(token: String): String = extractClaims(token).subject
+
+    private fun extractClaims(token: String): Claims = Jwts.parser()
+        .setSigningKey(properties.secretKey)
+        .parseClaimsJws(token)
+        .body
+
+    fun validateToken(token: String): Boolean = try {
+        extractClaims(token).expiration
+            .before(Date())
+            .not()
+    } catch (e: RuntimeException) {
+        false
+    }
+}
